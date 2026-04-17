b9b659e76c446d1e4fb18baabfa68e6d247a94bc
matvey popovich <matveypopovich2004@gmail.com>
2026-04-17 22:45:19 +0500

    Добавил миддваре и ллогирование в нем

--- app/main.py
+++ app/main.py
@@ -1,24 +1,20 @@
-from contextlib import asynccontextmanager
+import logging
+from  time import perf_counter
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.router import api_router
 from app.core.config import get_settings
 from app.models.base import Base
 from app.db.session import engine
+from app.core.logging import configure_logging
 
-
-# @asynccontextmanager
-# async def lifespan(_: FastAPI):
-#     # Все нужные модели должны быть импортированы перед запуском
-#     from app.models.task import TaskORM
-#     Base.metadata.create_all(bind=engine)
-#     yield
-
+configure_logging()
 
 settings = get_settings()
 app = FastAPI()
+logger = logging.getLogger("app.middleware")
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,4 +24,44 @@ app.add_middleware(
     allow_credentials=True,
 )
 
+@app.middleware("http")  # log_requests выполнится до и после обработки каждого HTTP-запроса
+async def log_requests(request: Request, call_next) -> Response:
+    print("мидллваре 1 работает")
+    started_at = perf_counter()
+    try:
+        response: Response = await call_next(request)  # Работа самого эндпоинта
+    except Exception:
+        duration_ms = (perf_counter() - started_at) * 1000
+        logger.exception(
+            "Request failed: %s %s completed_in=%.2fms",
+            request.method,
+            request.url.path,
+            duration_ms,
+        )
+        raise
+
+    duration_ms = (perf_counter() - started_at) * 1000
+    logger.info(
+        "%s %s -> %s (%.2f ms)",
+        request.method,
+        request.url.path,
+        response.status_code,
+        duration_ms,
+    )
+    return response
+
+
+request_count = 0 # здесь  считаем общее количество запросов
+
+@app.middleware("http")
+async def response_counter(request: Request, call_next) -> Response:
+    print("мидллваре 1 работает")
+    global request_count
+    request_count += 1
+    response: Response = await call_next(request)
+    response.headers['X-Request-Number'] = str(request_count)
+    return response
+
 app.include_router(api_router)
+
+
